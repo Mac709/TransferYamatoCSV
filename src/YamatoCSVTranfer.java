1ef5dc0dac1930e03c7abaf42cd8586f4bc1135f
Mac709 <justin.s.dje35284syeg@gmail.com>
2023-01-22 21:30:34 +0900

    upload

--- src/YamatoCSVTranfer.java
+++ src/YamatoCSVTranfer.java
@@ -1,118 +1,132 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class YamatoCSVTranfer {
+
   public static void main(String[] args) throws IOException {
     String line;
-    String[][] data = new String[500][100];
+
+// ファイルパス
+    Path path = Paths.get("trade.csv");
+// ファイルの行数
+    long lineCount = Files.lines(path).count();
+    String[][] data = new String[(int) lineCount][9];
+
     int i=0;
     try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("trade.csv"), "UTF-8"))) {
-   
       while ((line = br.readLine()) != null) {
         data[i] = line.split(",");
         i++;
       }
     } catch (IOException e) {
       System.out.print("ファイルが存在しませんでした。ファイルを準備してください。");
+    } catch ( ArrayIndexOutOfBoundsException | NullPointerException e ) {
+      System.out.println("配列の範囲外にアクセスしようとしました。");
     }
 
-    try (FileWriter file = new FileWriter("yamato.csv", false);
-        PrintWriter pw = new PrintWriter(new BufferedWriter(file))) {
+    try (
+        PrintWriter pw = new PrintWriter(new BufferedWriter
+                (new OutputStreamWriter(new FileOutputStream("yamato.csv"),"Shift-JIS")))) {
 
-      for (int a = 0; a < data.length; a++) {
-        pw.print(" ");
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print("2023/01/20");
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print(data[i][6]);//電話番号
-        pw.print(",");
-        pw.print(" ");
-        pw.print(",");
-        pw.print(data[i][4]);//郵便番号
-        pw.print(",");
-        pw.print(data[i][5]);//住所
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print(data[i][1]);
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print(data[i][3]);//氏名
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("#######");//依頼者電話番号
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("#####");//依頼者郵便番号
-        pw.print(",");
-        pw.print("#####");//依頼者住所
-        pw.print(",");
-        pw.print("#####");//依頼者組織名
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print(data[i][7]);
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("#####");//依頼者電話番号
-        pw.print(",");
-        pw.print("");
-        pw.print(",");
-        pw.print("1");
-        pw.println();
-        i++;
+      int num=2;
+      int countList=0;
+      while(data[num].length >4){
+        countList=num+1;
+        num++;
+      }
+
+      for (int a = 2; a < countList; a++) {
+          pw.print(" ");
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print("2023/01/20");
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print(data[a][6]);//電話番号
+          pw.print(",");
+          pw.print(" ");
+          pw.print(",");
+          pw.print(data[a][4]);//郵便番号
+          pw.print(",");
+          pw.print(data[a][5]);//住所
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print(data[a][1]);
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print(data[a][3]);//氏名
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print(data[a][7]);
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("569281163");
+          pw.print(",");
+          pw.print("");
+          pw.print(",");
+          pw.print("1");
+          pw.println();
       }
       pw.close();
       System.out.println("銘柄残高のCSVファイルをYamato用に変換しました。");
     } catch (IOException e) {
       System.out.println("ファイルを読み込むことができませんでした。ファイルを確認してください。");
+    } catch ( ArrayIndexOutOfBoundsException e ) {
+      System.out.println("data配列の範囲外にアクセスしようとしました。");
+    }catch (NullPointerException e ) {
+      System.out.println("nullの範囲外にアクセスしようとしました。");
     }
-
- 
   }
 }
